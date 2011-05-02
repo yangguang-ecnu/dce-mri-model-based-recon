bffa403e18d8955438ce371893741aa49135b8e4
ben.felsted@gmail.com <ben.felsted@gmail.com@77a2bb3f-a47c-3606-9e98-e01dabdd9fe4>
2011-05-02 17:17:11 +0000

    Removed

--- trunk/convolution/prototypeConvolution.m
+++ trunk/convolution/prototypeConvolution.m
@@ -54,8 +54,8 @@
 % g           Pixel intensity time series (g(x,t) in TMI paper)
 %
 
-function prototypeConvolution(~)
-    
+function prototypeConvolution(varargin)
+    step_test
     if ~nargin, demo, return, end
 
 end
@@ -84,9 +84,12 @@ function step_test
             y = y + 1;
         end
     end
+    %dbpause
     
     figure
     imagesc(C','XData',[0 X-1]-0/2,'YData',[0 Y-1]-0/2), axis xy
+    
+    %dbpause
     
     set(gca,'XTick', (0:X-1)-1/2)
     set(gca,'YTick', (0:Y-1)-1/2)
@@ -95,113 +98,11 @@ function step_test
     set(gca,'XColor',[0.5,0.6,0.8])
     set(gca,'YColor',[0.5,0.6,0.8])
     set(gca,'GridLineStyle', '-')    
-    '';
 end
 
-%% Demo
-%
 function demo
-%     
-%     close all
-%     
-%     step_test
-% 
-%     t = linspace(0,5,1000);
-%     Cp = AIF(t);
-%     
-% %     figure, plot(t,Cp)
-%     
-%     ti = linspace(t(1), t(end), 50);
-%     dti = ti(2)-ti(1);
-%     
-%     t0 = 1;
-% %     kEp = 5;
-% %     kTrans = 1/kEp;
-%     kEp = 5;
-%     kTrans = 1;
-%     %kTrans = 1/(1+1/kEp);
-%     %kTrans = 1/(1 + exp(-kEp*dti))*1/sqrt(2*dti);
-%     
-%     %e = kTrans * exp( -kEp*(t - t0) ) .* (t >= t0);
-%     e = kernel(t, kTrans, kEp, t0);
-%     figure, hold all; plot(t,e)
-%     
-%     E = kernel(t', kTrans, kEp, ti);
-%     Cpi = AIF(ti);
-%     
-%     figure('Position', [717   524   560   420])
-%     plot(repmat(t',[1 size(E,2)]), bsxfun(@times, E, Cpi)/kTrans)
-%     hold all
-%     plot(t,Cp)
-%     
-%     %figure('Position', [717   19   560   420])
-%     %plot(ti, sum(bsxfun(@times, E, Cpi)),'-o')
-%     
-%     figure('Position', [717   19   560   420])
-%     plot(ti, convolveCWithKernel(t, kTrans, kEp, ti))
-%     
-%     
-% %     hold all
-% %     plot(ti, convolveCWithKernel(ti, kTrans, kEp, ti),'-o')
-% %     plot(t, sum(bsxfun(@times, E, Cp)),'-')
-%     
-%     snapnow
-%     
-%     
-%     %% 
-%     %
-%     %
-%     f1 = @(x) AIF(x);
-%     f2 = @(x) kernel(x, kTrans, kEp, 0);
-%     %t = linspace(t(1),t(end),4000);
-%     q = quadv(@(tau) f2(tau) * f1(t-tau), t(1), t(end));
-%     
-%     figure, plot(t,q)
-%     hold all
-%     plot(ti, convolveCWithKernel(t, kTrans, kEp, ti)/300)
-%     
-%     snapnow
-%     
-%     keps = logspace(log10(0.05),log10(100),10);
-%     s = zeros(length(t),length(keps));
-%     for k = 1:length(keps)
-%         for i=1:length(t)
-%             s(i,k) = keps(k)*convolutionFromMaple(t(i), keps(k), 1, 1);
-%         end
-%     end
-%     figure, plot(t,s, 'LineWidth', 0.5)
-%     %print -depsc2 -r300 myfile.eps
-%     ti_old = ti;
-% 
-% 
-%     oversample_i = 4;
-%     oversample_j = 8;
-%     ti = linspace(ti_old(1),ti_old(end),oversample_i*length(ti_old));
-%     tj = linspace(ti_old(1),ti_old(end),oversample_j*length(ti_old));
-%     Cpj = AIF(tj);
-%     
-%     figure
-%     hold all
-%     plot(tj,Cpj*10,'LineWidth',5)
-%     
-%     for k_ep = logspace(log10(0.01),log10(1000),20);
-%         KTrans = k_ep;
-%         si = zeros(size(ti));
-%         for j=1:length(tj)
-%             si = si + Cpj(j) * KTrans * convolutionFromMapleVectorized(ti, k_ep, tj(j), oversample_j);
-%     %         for i=1:length(si)
-%     %             si(i) = si(i) + Cpi(j) * convolutionFromMaple(ti(i), 2, ti(j), oversample);
-%     %         end
-%         end
-%         plot(ti,si)
-%     end
-%     %figure, plot(ti,si)
-%     
-%     
     step_test
     convolutionDemo
-
-    '';
 end
 
 
